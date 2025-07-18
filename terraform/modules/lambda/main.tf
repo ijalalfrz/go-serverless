350f16950762fe53c22b6b4536cd2cbbf1ac217e
Rizal Alfarizi <rizal.alfarizi@bobobox.com>
2025-07-19 01:31:46 +0700

    feat: format tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,11 +1,11 @@
 resource "aws_lambda_function" "function" {
-  filename         = var.lambda_zip_path
-  function_name    = "${var.app_name}-${var.environment}"
-  role            = aws_iam_role.lambda_role.arn
-  handler         = "bootstrap"
-  runtime         = "provided.al2023"
-  memory_size     = var.memory_size
-  timeout         = var.timeout
+  filename      = var.lambda_zip_path
+  function_name = "${var.app_name}-${var.environment}"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "bootstrap"
+  runtime       = "provided.al2023"
+  memory_size   = var.memory_size
+  timeout       = var.timeout
 
   environment {
     variables = var.environment_variables
