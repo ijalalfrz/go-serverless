2e77154db3c9d6f7f44436b744a383661c1f636a
Rizal Alfarizi <rizal.alfarizi@bobobox.com>
2025-07-19 12:08:50 +0700

    feat: format tf

--- terraform/backend/main.tf
+++ terraform/backend/main.tf
@@ -41,9 +41,9 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
 
 # DynamoDB table for state locking
 resource "aws_dynamodb_table" "terraform_locks" {
-  name           = "go-serverless-terraform-locks"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
+  name         = "go-serverless-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
   attribute {
     name = "LockID"
